cbe4ed43ae32f0a0d56eab4ea5d7ed01dba23dce
Peagah <peagahvieira2003@gmail.com>
2023-07-23 11:28:35 -0300

    refactor: user - login and register test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -14,48 +14,54 @@ class LoginTest extends DuskTestCase
     protected function setUp(): void
     {
         parent::setUp();
+
+        // Freshing the database
         $this->artisan('migrate:fresh');
+
+        // Create a unverified email user for testing purposes
+        $this->user_unverified = User::factory()->create([
+            'name' => 'John Smith',
+            'email' => 'unverified@gmail.com',
+            'password' => 'Unver@ify12',
+            'email_verified_at' => null,
+        ]);
+
+        // Create a verified email user for testing purposes
+        $this->user_verified = User::factory()->create([
+            'name' => 'John Smith',
+            'email' => 'verified@gmail.com',
+            'password' => 'Ver@ify12',
+            'email_verified_at' => now(),
+        ]);
     }
 
     public function test_unverified_user_login_is_working_as_expected(): void
     {
         $string_password = 'Unver@ify12';
-
-        $user_unverified = User::factory()->create([
-            'email' => 'unverified@gmail.com',
-            'password' => $string_password,
-            'email_verified_at' => null,
-        ]);
-
-        $this->browse(function (Browser $browser) use ($user_unverified, $string_password){
-            $browser->visit('/login')
-                    ->type('email', $user_unverified->email)
-                    ->type('password', $string_password)
-                    ->click('button[type="submit"]')
-                    ->waitForLocation('/verify-email')
-                    ->assertsee('Thanks for signing up!')
-                    ->logout();
+        $this->browse(function (Browser $browser) use ($string_password) {
+            $browser
+                ->visit('/login')
+                ->type('email', $this->user_unverified->email)
+                ->type('password', $string_password)
+                ->click('button[type="submit"]')
+                ->waitForLocation('/verify-email')
+                ->assertsee('Thanks for signing up!')
+                ->logout();
         });
     }
 
     public function test_verified_user_login_is_working_as_expected(): void
     {
         $string_password = 'Ver@ify12';
-
-        $user_verified = User::factory()->create([
-            'email' => 'verified@gmail.com',
-            'password' => $string_password,
-            'email_verified_at' => now(),
-        ]);
-
-        $this->browse(function (Browser $browser) use ($user_verified, $string_password){
-            $browser->visit('/login')
-                    ->type('email', $user_verified->email)
-                    ->type('password', $string_password)
-                    ->click('button[type="submit"]')
-                    ->waitForLocation('/dashboard')
-                    ->assertSee("You're logged in!")
-                    ->logout();
+        $this->browse(function (Browser $browser) use ($string_password) {
+            $browser
+                ->visit('/login')
+                ->type('email', $this->user_verified->email)
+                ->type('password', $string_password)
+                ->click('button[type="submit"]')
+                ->waitForLocation('/dashboard')
+                ->assertSee("You're logged in!")
+                ->logout();
         });
     }
 }
--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -10,17 +10,26 @@ class RegisterTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Freshing the database
+        $this->artisan('migrate:fresh');
+    }
+
     public function test_user_register_is_working_as_expected(): void
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/register')
-                    ->type('name', 'John Smith')
-                    ->type('email', 'johnsmith@gmail.com')
-                    ->type('password', 'J@hn Sm3th')
-                    ->type('password_confirmation', 'J@hn Sm3th')
-                    ->click('button[type="submit"]')
-                    ->waitForLocation('/verify-email')
-                    ->assertsee('Thanks for signing up!');
+            $browser
+                ->visit('/register')
+                ->type('name', 'John Smith')
+                ->type('email', 'johnsmith@gmail.com')
+                ->type('password', 'J@hn Sm3th')
+                ->type('password_confirmation', 'J@hn Sm3th')
+                ->click('button[type="submit"]')
+                ->waitForLocation('/verify-email')
+                ->assertsee('Thanks for signing up!');
         });
     }
 }
