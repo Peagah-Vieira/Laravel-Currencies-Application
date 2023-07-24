b6da85ebcc2fbbd1031b3fe190ffd97343ad53ea
Peagah <peagahvieira2003@gmail.com>
2023-07-24 07:37:46 -0300

    refactor: User register browser test

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -10,19 +10,34 @@ class RegisterTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
+    const ROUTE_USER_REGISTER = '/register';
+
     protected function setUp(): void
     {
+        // Prerequisite steps
         parent::setUp();
 
         // Freshing the database
         $this->artisan('migrate:fresh');
     }
 
+    protected function tearDown(): void
+    {
+        // Logout the user
+        $this->browse(function (Browser $browser) {
+            $browser->logout();
+
+        });
+
+        // Clean-up steps
+        parent::tearDown();
+    }
+
     public function test_user_register_is_working_as_expected(): void
     {
         $this->browse(function (Browser $browser) {
             $browser
-                ->visit('/register')
+                ->visit(self::ROUTE_USER_REGISTER)
                 ->type('name', 'John Smith')
                 ->type('email', 'johnsmith@gmail.com')
                 ->type('password', 'J@hn Sm3th')
