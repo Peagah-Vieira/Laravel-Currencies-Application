c6ed8cf7d41aad55be190aabbacc0c8b2f7b787d
Peagah <peagahvieira2003@gmail.com>
2023-07-24 12:48:52 -0300

    feat: CurrencyController - convert_currencies method

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -15,8 +15,8 @@ class CurrencyController extends Controller
 
         $params = [
             'query' => [
-                'apikey' => env('CURRENCY_API_KEY'),
-             ]
+                'apikey' => env('CURRENCY_API_LIST_KEY'),
+            ],
         ];
 
         $response = $client->request('GET', $url, $params);
@@ -24,5 +24,32 @@ class CurrencyController extends Controller
         $response_body = json_decode($response->getBody());
 
         return view('currencies.list', compact('response_body'));
+    }
+
+    public function convert_currencies()
+    {
+        $headers = [
+            'X-RapidAPI-Key' => env('CURRENCY_API_CONVERT_KEY'),
+        ];
+
+        $client = new Client([
+            'headers' => $headers,
+        ]);
+
+        $url = env('CURRENCY_API_CONVERT_URL');
+
+        $params = [
+            'query' => [
+                'have' => 'USD',
+                'want' => 'EUR',
+                'amount' => 5000,
+            ],
+        ];
+
+        $response = $client->request('GET', $url, $params);
+
+        $response_body = json_decode($response->getBody());
+
+        return view('currencies.convert', compact('response_body'));
     }
 }
