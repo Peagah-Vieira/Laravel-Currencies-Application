28975770f584ef748b3f420004b05db589c0a73b
Peagah <peagahvieira2003@gmail.com>
2023-07-26 10:19:56 -0300

    feat: Theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import colors from "tailwindcss/colors";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
     ],
-
+    darkMode: "class",
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                danger: colors.rose,
+                primary: colors.sky,
+                success: colors.green,
+                warning: colors.yellow,
             },
         },
     },
-
-    plugins: [forms],
+    plugins: [forms, typography],
 };
